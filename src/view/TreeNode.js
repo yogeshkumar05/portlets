fc6ec95caf224aee1024172ae219a957454421d6
Yogesh <yogeshkumar.05@live.in>
2017-05-30 09:54:17 +0530

    removed classNames dependency

--- src/view/TreeNode.js
+++ src/view/TreeNode.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import classNames from 'classNames';
 import { addNodeAction } from './../actions/treeActions'
 import { DropdownButton, MenuItem, SplitButton, Modal, Button } from 'react-bootstrap';
 
